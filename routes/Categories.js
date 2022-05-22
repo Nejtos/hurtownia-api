b915538ba2b336b3633a72b1b8f3d0045061f459
nejtos <nejcik@interia.pl>
2022-05-22 18:17:17 +0200

    adding creating new rows in db

--- routes/Categories.js
+++ routes/Categories.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
 const Categories = require("../models/Categories");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+app.use(express.json());
 
 router.get("/",async (req, res) => {
     const listofCategories = await Categories.findAll();
     res.json(listofCategories);
+});
+
+// router.post("/create", async (req, res) => {
+//     const values = {
+//       id_kategorii: req.body.id_kategorii,
+//       // data: req.body.deliveries.data,
+//       nazwa_kategorii: req.body.nazwa_kategorii,
+//     };
+//     await Categories.create(values, {
+//       where: { id_kategorii: req.body.id_kategorii, nazwa_kategorii: req.body.nazwa_kategorii },
+//     });
+// });
+
+
+router.post("/create", jsonParser, (req, res) => {
+    const { id_kategorii, nazwa_kategorii } = req.body;
+    Categories.create({ id_kategorii: id_kategorii, nazwa_kategorii: nazwa_kategorii })
+    .then(() => {
+        res.json("Create new category");
+    })
+    .catch((err) => {
+      if (err) {
+        res.status(400).json({ error: err });
+      }
+    });
 });
 
 module.exports = router;
--- routes/Elements.js
+++ routes/Elements.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
 const Elements = require("../models/Elements");
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+app.use(express.json());
 
 router.get("/",async (req, res) => {
     const listofElements = await Elements.findAll();
     res.json(listofElements);
+});
+
+router.post("/create", jsonParser, (req, res) => {
+    const { id_elementu, nazwa_elementu } = req.body;
+    Elements.create({ id_elementu: id_elementu, nazwa_elementu: nazwa_elementu })
+    .then(() => {
+        res.json("Create new element");
+    })
+    .catch((err) => {
+      if (err) {
+        res.status(400).json({ error: err });
+      }
+    });
 });
 
 module.exports = router;
